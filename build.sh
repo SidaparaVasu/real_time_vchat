50f26dd7950b791f903060818734dae548787ea8
unknown <74949446+SidaparaVasu@users.noreply.github.com>
2025-02-03 23:51:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 
 pip install gunicorn
 
-export DJANGO_SETTINGS_MODULE = real_time_chatapp.settings
+set DJANGO_SETTINGS_MODULE = real_time_chatapp.settings
 
 python manage.py collectstatic --no-input
 python manage.py migrate
