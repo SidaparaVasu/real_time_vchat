c6af0be6cd163476d84fb73c24371ddc0c1e88ee
unknown <74949446+SidaparaVasu@users.noreply.github.com>
2025-02-03 22:52:09 +0530

    Updated settings.py
    * Set debug as False
    * Set host link to ALLOWED_HOSTS

--- real_time_chatapp/settings.py
+++ real_time_chatapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-942#rn+@e2y(c^$&)gre_=2ka%ls+7a-%wunm@s0^b&c52%#4u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*', 'https://real-time-chatapp-dbkh.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
 
