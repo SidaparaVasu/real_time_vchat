dded3571882ed0c94cc73f28ec032ca68473a9bc
unknown <74949446+SidaparaVasu@users.noreply.github.com>
2025-02-04 00:18:47 +0530

    Update settings.py

--- real_time_chatapp/asgi.py
+++ real_time_chatapp/asgi.py
@@ -8,14 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat import routing  
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_time_chatapp.settings')
-django.setup()
 
 # Initialize the ASGI application
 
