809a362b0386c9f9e06e4d8e5ae1090c1103b8e2
unknown <74949446+SidaparaVasu@users.noreply.github.com>
2025-02-04 00:08:58 +0530

    Update settings.py

--- real_time_chatapp/settings.py
+++ real_time_chatapp/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import django
+django.setup()
+
 import os
 from pathlib import Path
 from dotenv import load_dotenv
