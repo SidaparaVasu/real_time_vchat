c68ae0fd0a5ec225514118afc37df03ed813ad63
unknown <74949446+SidaparaVasu@users.noreply.github.com>
2025-02-04 13:18:48 +0530

    Update settings.py

--- real_time_chatapp/settings.py
+++ real_time_chatapp/settings.py
@@ -34,7 +34,7 @@ CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic'
+    # 'whitenoise.runserver_nostatic'
     'daphne',
     'channels',
     'django.contrib.admin',
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
