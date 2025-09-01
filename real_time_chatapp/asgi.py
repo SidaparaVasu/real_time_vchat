9df0a732308a26e11b1b4f981a3442d474635c34
Vasu Sidapara <vasupatel303@gmail.com>
2025-09-01 13:32:50 +0530

    Fixed asgi issue

--- real_time_chatapp/asgi.py
+++ real_time_chatapp/asgi.py
@@ -1,25 +1,12 @@
-"""
-ASGI config for real_time_chatapp project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
-import os
+import os, django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat import routing  
 
-import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_time_chatapp.settings')
 django.setup()
 
-# Initialize the ASGI application
-
-# application = get_asgi_application()
+from chat import routing  
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -29,5 +16,3 @@ application = ProtocolTypeRouter({
         )
     ),
 })
-
-# app = application
